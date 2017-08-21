64759a031e535431cba3ffb33d9dc3a859785d0c
zinza dev <dev@zinza.com.vn>
2017-08-21 17:36:00 +0700

    deploy 10 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,17 +2,18 @@
 lock "3.9.0"
 
 # set :application, "my_app_name"
-set :application, "activeadmin_rails"
+set :application, "my_app_name"
 # set :repo_url, "git@example.com:me/my_repo.git"
 set :repo_url, "https://github.com/trunghongoc/activeadmin_rails.git"
 
-set :deploy_to, '/var/www/activeadmin_rails'
+set :deploy_to, '/var/www/my_app_name'
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 set :branch,        :master
 set :user, "root"
 set :use_sudo, true
 set :rbenv_ruby, '2.4.0'
+set :bundle_flags, '--deployment'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
